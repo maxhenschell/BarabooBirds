871ab1ae94d61e3b81dcf7abc0a7b511a03e0a1c
Max Henschell <mahensch@gmail.com>
2019-03-22 11:45:35 -0400

    Data analysis 3/22/19
    Added PlotData, updated dataPrep

--- 01_dataPrep.R
+++ 01_dataPrep.R
@@ -20,16 +20,29 @@ lapply(packages.list, require, character.only=T)
 #Load bird data
 #
 
-birdCount = read_csv("bircCount.csv")
+birdCount = read_csv("birdCount.csv")
 landscape = read_csv("landscape.csv")
 guilds = read_csv("guildMembers.csv")
+plotData = read.csv("PlotData.csv") %>% arrange(PLOT)
 
-birdRA = read_csv()
+ls70 = landscape[landscape$Year == 1970,]%>% arrange(PLOT)
+ls00 = landscape[landscape$Year == 2000,]%>% arrange(PLOT)
+lsDiff = ls00[,-c(1:2)] - ls70[,-c(1:2)]; lsDiff = bind_cols(PLOT = plotData$PLOT, lsDiff) 
+lsDifflong = lsDiff %>% gather('Core250m':'House2000m', key='var', value='vals')
+lsPlot = ggplot(lsDifflong, aes(var,vals))
+lsPlot+geom_boxplot(notch = TRUE) + geom_jitter(width = 0.2) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-ls70 = landscape[landscape$Year == 1970,]
-ls00 = landscape[landscape$Year == 2000,]
-lsDiff = ls00[,-c(1:2)] - ls70[,-c(1:2)]; lsDiff = bind_cols(ls70[1,], lsDiff)
-
+tTestdf = data.frame(matrix(nrow = 16, ncol = 6));names(tTestdf) = c("var","stat","Pval","Est","CIlow","CIhigh")
+for(i in 3:ncol(landscape)){
+  tTestdf[i-2,1] = colnames(ls70[,i])
+  my_data <- data.frame( 
+    group = rep(c("ls70", "ls00"), each = nrow(ls70)),
+    var = rbind(ls70[,i],  ls00[,i])
+  )
+  tt = t.test(my_data[,2] ~ my_data[,1], data = my_data, paired = TRUE, alternative = "two.sided")
+  resid(tt)
+  tTestdf[i-2,2] = tt$statistic; tTestdf[i-2,3] = tt$p.value; tTestdf[i-2,4] = tt$estimate; tTestdf[i-2,5] = tt$conf.int[1]; tTestdf[i-2,6] = tt$conf.int[2]
+}
 
 
 guildList = data.frame(matrix(nrow = nrow(birdCount),ncol = 7))
